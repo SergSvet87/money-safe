f11090a5d4c64073f8b69a27731c3d0f67585010
SergSvet87 <svetlichnij1905@gmail.com>
2023-11-07 11:04:07 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+import { convertStringToNumber } from "./convertStringToNumber.js";
+
+const financeForm = document.querySelector(".finance__form");
+const financeAmount = document.querySelector(".finance__amount");
+const financeReport = document.querySelector(".finance__report");
+
+let amount = 0;
+financeAmount.textContent = amount;
+
+financeAmount.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const typeOperation = e.submitter.dataset.typeOperation;
+  const changeAmount = Math.abs(
+    convertStringToNumber(financeForm.amount.value)
+  );
+
+  if (typeOperation === "income") {
+    amount += changeAmount;
+  }
+
+  if (typeOperation === "expenses") {
+    amount -= changeAmount;
+  }
+
+  financeAmount.textContent = `${amount.toLocaleString()} ₴`;
+});
+
+financeReport.addEventListener("click", () => {
+  const reportBox = document.querySelector(".report");
+  reportBox.classList.add("report__open");
+});
